f1383e04437f59245ab88591fa64bb1c5bbd3937
einsteinium400 <sdana1933@gmail.com>
2021-04-29 13:51:00 +0300

    update

--- js/terminal.js
+++ js/terminal.js
@@ -14,85 +14,86 @@ const localEcho = new LocalEchoController(term);
 const writeSpeed = 10;
 
 // var mailreg_full = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-var mailreg = /.*@.*\..*/
+var mailReg = /.*@.*\..*/
 // var mailreg = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
 var urlreg = /[Hh][Tt][Tt][Pp][Ss]?:\/\/(?:(?:[a-zA-Z\u00a1-\uffff0-9]+-?)*[a-zA-Z\u00a1-\uffff0-9]+)(?:\.(?:[a-zA-Z\u00a1-\uffff0-9]+-?)*[a-zA-Z\u00a1-\uffff0-9]+)*(?:\.(?:[a-zA-Z\u00a1-\uffff]{2,}))(?::\d{2,5})?(?:\/[^\s]*)?/; //ALERT
 
 
-var longOpening = 'Hello hacker x.\r\nIf you found that page, it means we will check your skills to join\r\nShenkar software engineers team.\r\nPlease keep the whole process discrete and make sure you have a hoodie.\r\n';
+var longOpening = 'Hello hacker x.\r\nIf you found that page, it means we will check your skills to join\r\nShenkar software engineers team.\r\nPlease keep the whole process discrete and make sure you have a hoodie.\r\nGood luck.          \r\n';
 var opening = "hello\r\n";
 var questions = [{
-        //NAME
-        "prompt": "Hackers nickname",
-        "regex": /(?: [a-zA-Z]+){1,}/,
-        "form_id": "nickname",
-        "formValue": "value",
-        "wronginput": "! Invalid Input: name must include letters from a-z only and a gap"
-    },
-    {
-        //FAVORITE BROWSWE RADIO BUTTON SELECT GENDER
-        "prompt": "Favorite browser",
-        "type": "radio",
-        "form_id": "radio",
-        "formClass": "radio",
-        "formValue": "checked"
-    }, {
-        //COFFEE DAYS SINCE ALERT NOT WORKING
-        "prompt": "Days since last hack",
-        "type": "number",
-        // "form_id": "number",
-        "formClass": "number",
-    }, {
-        //PASSWORD
-        "prompt": "Penthagon's password",
-        "regex": /(?=.*[A-Z])/, //must contain at least one uppercase alphabetical character
-        "form_id": "passwordinput",
-        "formValue": "value",
-        "wronginput": "! Invalid Input: must contain at least one uppercase alphabetical character"
-    }, {
-        //MAIL
-        "prompt": "e-mail address in darknet",
-        "regex": mailreg,
-        "form_id": "mail",
-        "formValue": "value",
-        "wronginput": "! Invalid Input: must include '@' and '.' symbols"
-    }, {
-        //URL
-        "prompt": "Last website I hacked",
-        "regex": urlreg,
-        "form_id": "website",
-        "formValue": "value",
-        "wronginput": "! Invalid Input: URL should look like http(s)://(www.)domain.dns"
-    }, {
-        //PHONE
-        "prompt": "Phone number of last victim",
-        "regex": /[0-9]{9,10}/,
-        "form_id": "phone",
-        "formValue": "value",
-        "wronginput": "! Invalid Input:9-10 numbers only"
+    //NAME
+    "prompt": "Hackers nickname",
+    "regex": /(?: [a-zA-Z]+){1,}/,
+    "form_id": "nickname",
+    "formValue": "value",
+    "wronginput": "! Invalid Input: name must include letters from a-z only and a space"
+}, {
+    //PASSWORD
+    "prompt": "Penthagon's password",
+    "regex": /(?=.*[A-Z])/, //must contain at least one uppercase alphabetical character
+    "form_id": "passwordinput",
+    "formValue": "value",
+    "wronginput": "! Invalid Input: must contain at least one uppercase alphabetical character"
+},
+{
+    "prompt": "Programming languages",
+    "type": "checkbox",
+    "formClass": "checkbox",
+    "wronginput": "at least one must be checked"
+},
+{
+    "prompt": "IP Subnet Mask",
+    "type": "select",
+    "formClass": "subnetMask",
+},
+{
+    //FAVORITE BROWSWE RADIO BUTTON SELECT GENDER
+    "prompt": "Favorite browser",
+    "type": "radio",
+    "form_id": "radio",
+    "formClass": "radio",
+    "formValue": "checked"
+}, {
+    //COFFEE DAYS SINCE ALERT NOT WORKING
+    "prompt": "Days since last hack",
+    "type": "number",
+    // "form_id": "number",
+    "formClass": "number",
+}, {
+    "prompt": "Security rank",
+    "type": "range",
+    "formClass": "securityrank",
+    "formValue": "value",
+}, {
+    //MAIL
+    "prompt": "e-mail address in darknet",
+    "regex": mailReg,
+    "form_id": "mail",
+    "formValue": "value",
+    "wronginput": "! Invalid Input: must include '@' and '.' symbols"
+}, {
+    //URL
+    "prompt": "Last website I hacked",
+    "regex": urlreg,
+    "form_id": "website",
+    "formValue": "value",
+    "wronginput": "! Invalid Input: URL should look like http(s)://(www.)domain.dns"
+}, {
+    //PHONE
+    "prompt": "Phone number of last victim",
+    "regex": /[0-9]{9,10}/,
+    "form_id": "phone",
+    "formValue": "value",
+    "wronginput": "! Invalid Input:9-10 numbers only"
 
-    }, {
-        "prompt": "Security rank",
-        "type": "range",
-        "formClass": "securityrank",
-        "formValue": "value",
-    }, {
-        "prompt": "IP address",
-        "regex": /.*/g, // Match anything
-        "form_id": "address",
-        "wronginput": "wronginput",
-        "formValue":"value"
-    },
-    {
-        "prompt": "Programming languages",
-        "type": "checkbox",
-        "formClass": "checkbox",
-    },
-    {
-        "prompt": "IP Subnet Mask",
-        "type": "select",
-        "formClass": "subnetMask",
-    }
+}, {
+    "prompt": "IP address",
+    "regex": /.*/g, // Match anything
+    "form_id": "address",
+    "wronginput": "wronginput",
+    "formValue": "value"
+}
 ];
 
 
@@ -106,7 +107,7 @@ var showText = async function (message, index, interval) {
 };
 
 var handle_question = async function (question) {
-    await showText('~$ '+question.prompt, 0, writeSpeed); //print question to screen
+    await showText('~$ ' + question.prompt, 0, writeSpeed); //print question to screen
     let valid = false; //the input is not ok- assumption
     while (!valid) {
         input = await localEcho.read(': ', '~$ ' + question.prompt);
@@ -115,16 +116,16 @@ var handle_question = async function (question) {
             await showText(question.wronginput, 0, writeSpeed)
         }
     }
-    console.log(document.getElementById(question.form_id)[question.formValue]);
+    // console.log(document.getElementById(question.form_id)[question.formValue]);
     document.getElementById(question.form_id)[question.formValue] = input
 };
 
 var custom_question = async function (question) {
 
-    await showText('~$ '+question.prompt, 0, writeSpeed);
+    await showText('~$ ' + question.prompt, 0, writeSpeed);
     var rows = document.getElementsByClassName('xterm-rows')[0]
     var row = document.createElement('div')
-    var termSpan=term.renderer._spanElementObjectPool.acquire()
+    var termSpan = term.renderer._spanElementObjectPool.acquire()
 
     var formClass = document.getElementsByClassName(question.formClass)
 
@@ -136,25 +137,49 @@ var custom_question = async function (question) {
     termSpan.append(...formClass)
 
     row.appendChild(termSpan)
-  
+
     var cursor = document.getElementsByClassName('terminal-cursor')
 
     var data_ids = []
 
-    for (child of cursor[0].parentElement.children){
+    for (child of cursor[0].parentElement.children) {
         data_ids.push(child.getAttribute('data-obj-id'))
     }
-    term.renderer._spanElementObjectPool._inUse[data_ids[2]].innerHTML=''
-    cursor[0].parentElement.children[cursor[0].parentElement.children.length-1].innerHTML=''
-    row=cursor[0].parentElement.insertAdjacentElement('afterend', row)
+    term.renderer._spanElementObjectPool._inUse[data_ids[2]].innerHTML = ''
+    cursor[0].parentElement.children[cursor[0].parentElement.children.length - 1].innerHTML = ''
+    row = cursor[0].parentElement.insertAdjacentElement('afterend', row)
     input = await localEcho.read(': ', '~$ ' + question.prompt); //waiting for enter to be pressed
-    cursor[0].parentElement.children[cursor[0].parentElement.children.length-1].innerHTML=''
+
+    if (question.type == "checkbox") {
+
+        var sum = 0;
+        for (var ischeck of document.getElementsByClassName("checkbox")) {
+            if (ischeck.checked == true) {
+                sum++
+            }
+        }
+
+        while (sum == 0) {
+            await showText(question.wronginput, 0, writeSpeed)
+            input = await localEcho.read(': ', '~$ ' + question.prompt); //waiting for enter to be pressed  
+
+            for (var ischeck of document.getElementsByClassName("checkbox")) {
+                if (ischeck.checked == true) {
+                    sum++
+                }
+
+            }
+        }
+    }
+    cursor[0].parentElement.children[cursor[0].parentElement.children.length - 1].innerHTML = ''
 
     var formClone = termSpan.cloneNode(true)
     for (node of formClone.childNodes) {
         node.style.display = "none"
     }
-    document.getElementById("myForm").insertAdjacentElement('beforeend', formClone)    
+    document.getElementById("myForm").insertAdjacentElement('beforeend', formClone)
+    row.remove();
+    
 }
 
 window.onload = async () => {
@@ -163,19 +188,20 @@ window.onload = async () => {
     term.viewport.terminal.selectionManager.disable();
 
     await showText(longOpening, 0, writeSpeed);
+
+    term.clear();
     for (question of questions) {
+
         if (!question.type) {
             await handle_question(question); //for regular text questions
         } else {
-            console.log("custom");
             await custom_question(question); //for special ones like radio
         }
     }
-
-    await showText("~$ !@#$%^&*!@#$%^&*^%$#@!*&^%$#@! Hacking using your information now..", 0, writeSpeed);
+    term.clear();
+    await showText("~$ !@#$%^&*!@#$%^&*^%$#@!*&^%$#@! \r\n Hacking using your information now..", 0, writeSpeed);
     document.getElementById("myForm").submit();
 };
-
 
 
 // IN ORDER TO DISABLE THE SELECTION FUNCTIONALITY OF XTERMJS
